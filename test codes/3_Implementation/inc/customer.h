181d987e760704a36d00e66f408e77ea99809cd7
Kunal Mozarkar <kunalmozarkar@gmail.com>
2021-09-04 13:30:43 +0530

    commit

---
+++ test codes/3_Implementation/inc/customer.h
@@ -0,0 +1,18 @@
+#ifndef __CUSTOMER_H
+#define __CUSTOMER_H
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Customer
+{
+public:
+char name[100];
+char address[100];
+char phone[12];
+char from_date[20];
+char to_date[20];
+float payment_advance;
+int booking_id;
+};
+#endif
